351a1bacc3b11e162da8bf1399b5633f1d5384b0
Luke Ramljak <lukeramljak@proton.me>
2023-09-22 22:24:16 +0800

    add event listeners and calculations

--- tip-calculator-app/index.js
+++ tip-calculator-app/index.js
@@ -1,13 +1,65 @@
-const billAmount = document.getElementById("bill-amount");
+const billInput = document.getElementById("bill-amount");
 const tipButtons = document.querySelectorAll("[data-percentage]");
 const customTipButton = document.getElementById("custom-tip");
-const people = document.getElementById("people");
+const peopleInput = document.getElementById("people");
 const tipAmount = document.getElementById("tip-amount");
 const total = document.getElementById("total");
 const resetButton = document.getElementById("reset-button");
 
+window.addEventListener("DOMContentLoaded", () => {
+  resetValues();
+});
+
+function resetValues() {
+  billInput.value = null;
+  customTipButton.value = null;
+  peopleInput.value = null;
+  tipAmount.textContent = "$0.00";
+  total.textContent = "$0.00";
+}
+
+let billAmount = 0;
+let selectedPercentage = 0;
+let people = 0;
+
+function updateTotals() {
+  const tip = billAmount * selectedPercentage;
+  const tipPerPerson = tip / people;
+  const totalPerPerson = (billAmount + tip) / people;
+
+  // TODO: figure out why this returns infinity
+
+  tipAmount.textContent = `\$${tipPerPerson.toFixed(2)}`;
+  total.textContent = `\$${totalPerPerson.toFixed(2)}`;
+}
+
+billInput.addEventListener("input", () => {
+  billAmount = parseInt(billInput.value);
+  updateTotals();
+});
+
 tipButtons.forEach((button) => {
-  button.addEventListener("click", () =>
-    console.log(button.dataset.percentage)
-  );
+  button.addEventListener("click", () => {
+    tipButtons.forEach((otherButtons) => {
+      otherButtons.classList.remove("selected");
+    });
+    button.classList.add("selected");
+    selectedPercentage = button.getAttribute("data-percentage") / 100;
+    updateTotals();
+  });
+});
+
+// TODO: disable tip buttons if custom tip entered
+
+customTipButton.addEventListener("input", () => {
+  selectedPercentage = parseFloat(customTipButton.value) / 100;
+  updateTotals();
+});
+
+peopleInput.addEventListener("input", () => {
+  people = parseInt(peopleInput.value);
+});
+
+resetButton.addEventListener("click", () => {
+  resetValues();
 });
