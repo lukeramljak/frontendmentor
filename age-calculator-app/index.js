02b217df704031bd548dd6ba397be15c61f43e58
Luke Ramljak <lukeramljak@proton.me>
2023-09-25 19:25:24 +0800

    handle errors with separate functions

--- age-calculator-app/index.js
+++ age-calculator-app/index.js
@@ -4,29 +4,52 @@ submitButton.addEventListener("click", () => {
   validateFields();
 });
 
-function validateFields() {
+function checkForEmpty() {
   const inputFields = document.querySelectorAll(".input-item input");
 
   inputFields.forEach((field) => {
-    const label = field.parentElement.querySelector(".input-label");
-    const errorMessage = document.createElement("span");
-    const existingError = field.parentElement.querySelector(".error-message");
-
-    errorMessage.classList.add("error-message");
-    errorMessage.textContent = "This field is required";
-
-    if (field.value === "" || field.value === "0") {
-      label.classList.add("error-label");
-      field.classList.add("error-input");
-      if (!existingError) {
-        field.insertAdjacentElement("afterend", errorMessage);
-      }
+    if (field.value === "") {
+      addErrors(field, "This field is required");
     } else {
-      label.classList.remove("error-label");
-      field.classList.remove("error-input");
-      if (existingError) {
-        existingError.remove();
-      }
+      removeErrors(field);
     }
   });
+}
+
+function addErrors(field, error) {
+  const label = field.parentElement.querySelector(".input-label");
+  const errorMessage = document.createElement("span");
+  const existingError = field.parentElement.querySelector(".error-message");
+
+  errorMessage.classList.add("error-message");
+  errorMessage.textContent = error;
+
+  label.classList.add("error-label");
+  field.classList.add("error-input");
+  if (!existingError) {
+    field.insertAdjacentElement("afterend", errorMessage);
+  }
+}
+
+function removeErrors(field) {
+  const label = field.parentElement.querySelector(".input-label");
+  const existingError = field.parentElement.querySelector(".error-message");
+
+  label.classList.remove("error-label");
+  field.classList.remove("error-input");
+  if (existingError) {
+    existingError.remove();
+  }
+}
+
+function validateDateField() {
+  const day = parseInt(document.getElementById("day").value);
+  if (day < 1 || day > 31) {
+    console.log("works");
+  }
+}
+
+function validateFields() {
+  checkForEmpty();
+  validateDateField();
 }
