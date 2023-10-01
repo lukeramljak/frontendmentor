4256f0906c3c1f56289af58072af313e4614cf01
Luke Ramljak <lukeramljak@proton.me>
2023-10-01 15:52:31 +0800

    add better form validation

--- newsletter-signup-form/index.js
+++ newsletter-signup-form/index.js
@@ -1,13 +1,41 @@
-function validateForm(event) {
-  const emailField = document.getElementById("email");
-  const errorLabel = document.getElementById("error-label");
-  if (emailField.validity.typeMismatch) {
-    event.preventDefault();
-    emailField.classList.add("invalid");
-    errorLabel.classList.remove("hidden");
-  }
-}
+const form = document.getElementById("form");
+const email = document.getElementById("email");
 
-const form = document.querySelector("form");
-form.noValidate = true;
-form.addEventListener("submit", validateForm);
+const setError = (element, message) => {
+  const inputContainer = element.parentElement;
+  const errorDisplay = document.querySelector(".error");
+
+  errorDisplay.textContent = message;
+  inputContainer.classList.add("invalid");
+};
+
+const setValid = (element) => {
+  const inputContainer = element.parentElement;
+  const errorDisplay = document.querySelector(".error");
+
+  errorDisplay.textContent = "";
+  inputContainer.classList.remove("invalid");
+};
+
+const isValidEmail = (email) => {
+  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  return re.test(String(email).toLowerCase());
+};
+
+const validateForm = () => {
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, "Valid email required");
+  } else {
+    setValid(email);
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateForm();
+});
